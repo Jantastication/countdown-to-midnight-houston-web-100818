1259d16acfa8c3fa113090b935f9fc162e91bc3f
Jantastication O <georginaoregx@gmail.com>
2018-10-02 17:51:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,14 @@ def countdown(number)
       puts "#{number} SECOND(S)!"
       number -= 1
     end
-  returns  "HAPPY NEW YEAR!"
-  end
+  return  "HAPPY NEW YEAR!"
+end
 
   def countdown_with_sleep(number)
     while number > 0
-      number -= 1
+      puts "#{number} SECOND(S)!"
       sleep(1)
+      number -= 1
     end
      "HAPPY NEW YEAR!"
   end
